24b994bb713c0b631ebd1f53b18ed26952923de6
Muthupetchi <82134535+Muthupetchi2301@users.noreply.github.com>
2021-08-15 14:10:01 +0530

    Add files via upload

---
+++ print string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char a[100];
+    fgets(a,sizeof(a),stdin);
+    puts(a);
+    return 0;
+}
+© 2021 GitHub, Inc.
